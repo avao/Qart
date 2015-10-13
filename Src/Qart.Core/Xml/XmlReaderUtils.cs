2100397de236d4cd9a5706e1586ff648e2749e2d
avao <dev@avao.co>
2015-10-13 22:29:42 +0100

    XmlReaderUtils

---
+++ Src/Qart.Core/Xml/XmlReaderUtils.cs
@@ -0,0 +1,22 @@
+﻿using Qart.Core.Io;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Qart.Core.Xml
+{
+    public static class XmlReaderUtils
+    {
+        public static void UsingXmlReader(string fileName, Action<XmlReader> action)
+        {
+            using (var stream = FileUtils.OpenFileStreamForWriting(fileName))
+            using (var reader = XmlReader.Create(stream))
+            {
+                action(reader);
+            }
+        }
+    }
+}
