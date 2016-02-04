4621e9d2f7a27d3ee7fedaffe974d0fa8b631d73
avao <dev@avao.co>
2016-02-04 07:50:31 +0000

    LogManager added to container. Minor

--- Src/Qart.CyberTester/Bootstrapper.cs
+++ Src/Qart.CyberTester/Bootstrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
+using Common.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,6 +16,7 @@ namespace Qart.CyberTester
         public static WindsorContainer CreateContainer()
         {
             var container = new WindsorContainer();
+            container.Register(Component.For<ILogManager>().ImplementedBy<LogManager>());
             container.Install(FromAssembly.InDirectory(new AssemblyFilter(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location))));
             return container;
         }
---
+++ Src/TestApp/TestCaseHandler.cs
@@ -0,0 +1,27 @@
+﻿using Castle.MicroKernel.Registration;
+using Qart.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class TestCaseHandlerWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(Component.For<ITestCaseProcessor>().ImplementedBy<TestCaseHandler>().Named("blah"));
+        }
+    }
+
+    public class TestCaseHandler : ITestCaseProcessor
+    {
+
+        public void Process(TestCase testCase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
