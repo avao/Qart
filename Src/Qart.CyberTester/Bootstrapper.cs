1d49e11a7a792c34d1e729f89bb31219abe071f6
avao <dev@avao.co>
2016-08-02 08:29:47 +0100

    get rid of StreamTransformerResolver.

--- Src/Qart.CyberTester/Bootstrapper.cs
+++ Src/Qart.CyberTester/Bootstrapper.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
 
 namespace Qart.CyberTester
 {
@@ -20,17 +21,20 @@ namespace Qart.CyberTester
         public static WindsorContainer CreateContainer(IDataStore testsDataStore)
         {
             var container = new WindsorContainer();
-            container.Register(Component.For<ILogManager>().ImplementedBy<LogManager>());
-            container.Register(Component.For<ITestCaseLoggerFactory>().ImplementedBy<TestCaseLoggerFactory>());
-            container.Register(Component.For<ITestCaseProcessorInfoExtractor>().ImplementedBy<TestCaseProcessorInfoExtractor>());
-            container.Register(Component.For<IDataStore>().Instance(testsDataStore).Named("testsDataStore"));
-            container.Register(Component.For<ITestSystem>().ImplementedBy<TestSystem>());
+            var kernel = container.Kernel;
+            kernel.Register(Component.For<ILogManager>().ImplementedBy<LogManager>());
+            kernel.Register(Component.For<ITestCaseLoggerFactory>().ImplementedBy<TestCaseLoggerFactory>());
+            kernel.Register(Component.For<ITestCaseProcessorInfoExtractor>().ImplementedBy<TestCaseProcessorInfoExtractor>());
+            kernel.Register(Component.For<IDataStore>().Instance(testsDataStore).Named("testsDataStore"));
+            kernel.Register(Component.For<ITestSystem>().ImplementedBy<TestSystem>());
             
-            container.Register(Component.For<ITestCaseProcessorResolver>().Instance(new TestCaseProcessorResolver(container)));
-            container.Register(Component.For<IStreamTransformerResolver>().Instance(new StreamTransformerResolver(container)));
-            container.Register(Component.For<IContentProcessor>().ImplementedBy<ContentProcessor>());
-            container.Register(Component.For<IStreamTransformer>().ImplementedBy<ConcatStreamTransformer>().Named("concat"));
-            container.Register(Component.For<IStreamTransformer>().ImplementedBy<RefStreamTransformer>().Named("ref"));
+            kernel.Register(Component.For<ITestCaseProcessorResolver>().Instance(new TestCaseProcessorResolver(container)));
+
+            //content/stream transformation
+            kernel.Register(Component.For<IStreamTransformerResolver>().AsFactory());
+            kernel.Register(Component.For<IContentProcessor>().ImplementedBy<ContentProcessor>());
+            kernel.Register(Component.For<IStreamTransformer>().ImplementedBy<ConcatStreamTransformer>().Named("concat"));
+            kernel.Register(Component.For<IStreamTransformer>().ImplementedBy<RefStreamTransformer>().Named("ref"));
                         
             container.Install(FromAssembly.InDirectory(new AssemblyFilter(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location))));
             return container;
