44d329281ba1f4c6799752fa117da622b0c14b74
avao <dev@avao.co>
2017-04-15 19:18:46 +0100

    DummyTagProvider with registration.

---
+++ Src/Qart.Testing/DummyTagProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Qart.Testing
+{
+    public class DummyTagProvider : ITagProvider
+    {
+        public IEnumerable<string> GetTags(TestCase testCase)
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
