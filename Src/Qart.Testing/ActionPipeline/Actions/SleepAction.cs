11d00f76e84f6de7e58808a652426588b79c9c3d
avao <dev@avao.co>
2017-04-28 18:47:18 +0100

    SleepAction

---
+++ Src/Qart.Testing/ActionPipeline/Actions/SleepAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Qart.Testing.Framework;
+using System.Threading;
+
+namespace Qart.Testing.ActionPipeline.Actions
+{
+    public class SleepAction<T> : IPipelineAction<T>
+    {
+        private readonly int _durationMs;
+
+        public SleepAction(int durationMs)
+        {
+            _durationMs = durationMs;
+        }
+
+        public void Execute(TestCaseContext testCaseContext, T context)
+        {
+            testCaseContext.DescriptionWriter.AddNote("SleepAction", _durationMs.ToString() + " ms");
+            Thread.Sleep(_durationMs);
+        }
+    }
+}
