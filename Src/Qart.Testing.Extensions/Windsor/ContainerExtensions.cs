6dca25f02019e21719aa725b48cc51bb986d4bd0
avao <dev@avao.co>
2017-05-06 16:55:38 +0100

    - HttpClientExtensions - ContainerExtensions

---
+++ Src/Qart.Testing.Extensions/Windsor/ContainerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace Qart.Testing.Extensions.Windsor
+{
+    public static class ContainerExtensions
+    {
+        public static void RegisterPipelineAction<T, C>(this IKernel kernel, string name)
+            where T : IPipelineAction<C>
+        {
+            kernel.Register(Component.For<IPipelineAction<C>>().ImplementedBy<T>().Named(name).LifeStyle.Transient);
+        }
+    }
+}
