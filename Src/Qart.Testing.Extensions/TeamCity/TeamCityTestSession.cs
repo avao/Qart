e214848bd9f3565f59d7fdb503ee2bd6c11c02f1
avao <dev@avao.co>
2017-07-10 11:51:34 +0100

    Logging of exception data

--- Src/Qart.Testing.Extensions/TeamCity/TeamCityTestSession.cs
+++ Src/Qart.Testing.Extensions/TeamCity/TeamCityTestSession.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using System.Text;
+using System.Collections;
 
 namespace Qart.Testing.Extensions.TeamCity
 {
@@ -29,7 +31,15 @@ namespace Qart.Testing.Extensions.TeamCity
                 }
                 else
                 {
-                    logger.Info(_formatter.FormatMessage("testFailed", commonProperties.Concat(new[] { new ServiceMessageProperty("message", result.Exception.Message), new ServiceMessageProperty("details", result.Exception.StackTrace) })));
+                    var detailsBuilder = new StringBuilder(result.Exception.StackTrace);
+                    foreach (DictionaryEntry item in result.Exception.Data)
+                    {
+                        detailsBuilder.AppendLine();
+                        detailsBuilder.Append(item.Key);
+                        detailsBuilder.Append(" : ");
+                        detailsBuilder.Append(item.Value);
+                    }
+                    logger.Info(_formatter.FormatMessage("testFailed", commonProperties.Concat(new[] { new ServiceMessageProperty("message", result.Exception.Message), new ServiceMessageProperty("details", detailsBuilder.ToString()) })));
                 }
             }
             logger.Info(_formatter.FormatMessage("testFinished", commonProperties));
