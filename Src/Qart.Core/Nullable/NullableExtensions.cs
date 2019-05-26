6dbbc8e8f457e32eb13257c3ae940d1ba78f31b1
avao <dev@avao.co>
2019-05-26 10:00:32 +0100

    Nullable extensions

---
+++ Src/Qart.Core/Nullable/NullableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Qart.Core.Nullable
+{
+    public static class NullableExtensions
+    {
+        public static bool TryGet<T>(this T? nullable, out T value)
+            where T : struct
+        {
+            if (nullable.HasValue)
+            {
+                value = nullable.Value;
+                return true;
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
