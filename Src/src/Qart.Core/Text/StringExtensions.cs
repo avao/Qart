eea3b0bfe3c31f085c556c77ad48dddebfd1e74d
avao <dev@avao.co>
2019-12-19 10:19:15 +0000

    Between, SplitOnFirst string extensions UTs

--- Src/src/Qart.Core/Text/StringExtensions.cs
+++ Src/src/Qart.Core/Text/StringExtensions.cs
@@ -46,14 +46,40 @@ namespace Qart.Core.Text
             return value.Substring(0, pos);
         }
 
+        public static string Between(this string value, string leftToken, string rightToken)
+        {
+            var leftPos = value.IndexOf(leftToken, StringComparison.InvariantCulture);
+            if (leftPos == -1)
+            {
+                throw new ArgumentException("String does not contain requested token [" + leftToken + "]");
+            }
+
+            var rightPos = value.IndexOf(rightToken, leftPos + leftToken.Length, StringComparison.InvariantCulture);
+            if (rightPos == -1)
+            {
+                throw new ArgumentException("String does not contain requested token [" + rightToken + "]");
+            }
+
+            int startPos = leftPos + leftToken.Length;
+
+            return value.Substring(startPos, rightPos - startPos);
+        }
+
         public static (string, string) SplitOnLast(this string value, string token)
         {
             var pos = value.LastIndexOf(token, StringComparison.InvariantCulture);
-            if (pos == -1)
-            {
-                return (value, null);
-            }
-            return (value.Substring(0, pos), value.Substring(pos + 1));
+            return value.SplitOnIndex(pos, token.Length);
+        }
+
+        public static (string, string) SplitOnFirst(this string value, string token)
+        {
+            var pos = value.IndexOf(token, StringComparison.InvariantCulture);
+            return value.SplitOnIndex(pos, token.Length);
+        }
+
+        private static (string, string) SplitOnIndex(this string value, int index, int tokenLength)
+        {
+            return (value.Substring(0, index), value.Substring(index + tokenLength));
         }
 
 
---
+++ Src/tests/Qart.Core.Tests/Text/StringExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using Qart.Core.Text;
+
+namespace Qart.Core.Tests.Text
+{
+    class StringExtensionsTests
+    {
+        [TestCase("a.bc", ".", ExpectedResult = "a")]
+        [TestCase("abc.", ".", ExpectedResult = "abc")]
+        [TestCase(".ab", ".", ExpectedResult = "")]
+        [TestCase("ab<tag<tag>cd", "<tag>", ExpectedResult = "ab<tag")]
+        public string LeftOfSucceeds(string value, string token)
+        {
+            return value.LeftOf(token);
+        }
+
+        [TestCase("a.bc", ".", ExpectedResult = "bc")]
+        [TestCase("abc.", ".", ExpectedResult = "")]
+        [TestCase(".ab", ".", ExpectedResult = "ab")]
+        [TestCase("ab<tag<tag>cd", "<tag>", ExpectedResult = "cd")]
+        public string RightOfSucceeds(string value, string token)
+        {
+            return value.RightOf(token);
+        }
+
+        [TestCase("abc.cd", ".", "abc", "cd")]
+        [TestCase("abc.", ".", "abc", "")]
+        [TestCase(".cd", ".", "", "cd")]
+        [TestCase("<t>ab", "<t>", "", "ab")]
+        [TestCase("ab<t>c", "<t>", "ab", "c")]
+        [TestCase("abc<t>", "<t>", "abc", "")]
+        [TestCase("a.bc.cd", ".", "a.bc", "cd")]
+        [TestCase("abc..", ".", "abc.", "")]
+        [TestCase(".c.d", ".", ".c", "d")]
+        public void SplitOnLastSucceeds(string value, string token, string left, string right)
+        {
+            (var leftActual, var rightActual) = value.SplitOnLast(token);
+            Assert.That(leftActual, Is.EqualTo(left));
+            Assert.That(rightActual, Is.EqualTo(right));
+        }
+
+        [TestCase("abc.cd", ".", "abc", "cd")]
+        [TestCase("abc.", ".", "abc", "")]
+        [TestCase(".cd", ".", "", "cd")]
+        [TestCase("<t>ab", "<t>", "", "ab")]
+        [TestCase("ab<t>c", "<t>", "ab", "c")]
+        [TestCase("abc<t>", "<t>", "abc", "")]
+        [TestCase("a.bc.cd", ".", "a", "bc.cd")]
+        [TestCase("abc..", ".", "abc", ".")]
+        [TestCase(".c.d", ".", "", "c.d")]
+        public void SplitOnFirstSucceeds(string value, string token, string left, string right)
+        {
+            (var leftActual, var rightActual) = value.SplitOnFirst(token);
+            Assert.That(leftActual, Is.EqualTo(left));
+            Assert.That(rightActual, Is.EqualTo(right));
+        }
+
+        [TestCase("ab{c}d", "{", "}", ExpectedResult = "c")]
+        [TestCase("ab{}d", "{", "}", ExpectedResult = "")]
+        [TestCase("{ab}cd", "{", "}", ExpectedResult = "ab")]
+        [TestCase("a{bcd}{}", "{", "}", ExpectedResult = "bcd")]
+        [TestCase("a<tag>bc</tag>", "<tag>", "</tag>", ExpectedResult = "bc")]
+        [TestCase("a,,c", ",", ",", ExpectedResult = "")]
+        [TestCase(",a,b", ",", ",", ExpectedResult = "a")]
+        public string BetweenSucceeds(string value, string leftToken, string rightToken)
+        {
+            return value.Between(leftToken, rightToken);
+        }
+
+        [TestCase("ab.c..", ExpectedResult = "ab,c,,")]
+        [TestCase("", ExpectedResult = "")]
+        [TestCase("ab", ExpectedResult = "ab")]
+        public string ToCsvSucceeds(string value)
+        {
+            return value.Split(".").ToCsv();
+        }
+    }
+}
