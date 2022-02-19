9735c328a34d4558b9381c84880a42d7a7ff4d0f
avao <dev@avao.co>
2022-02-19 19:41:57 +0000

    VariableResover extended to support formatting.

--- src/Qart.Core/Text/VariableResolver.cs
+++ src/Qart.Core/Text/VariableResolver.cs
@@ -8,40 +8,44 @@ namespace Qart.Core.Text
     public static class VariableResolver
     {
         public static string Resolve(string value, IDictionary<string, string> tokens)
-        {
-            return Resolve(value, token => tokens[token]);
-        }
+            => Resolve(value, token => tokens[token]);
 
-        public static string Resolve(string value, Func<string, string> resolverFunc)
+        public static string Resolve(string value, Func<string, object> resolverFunc, IFormatProvider formatProvider = null)
+            => Resolve(value, (token, format) => string.Format(formatProvider, "{0" + format + "}", resolverFunc(token)));
+
+        public static string Resolve(string value, Func<string, string, string> resolverFunc)
         {
             const char prefixToken = '$';
             const char openingToken = '{';
             const char closingToken = '}';
 
-            var dollarPos = value.IndexOf(prefixToken);
-            if (dollarPos == -1)
+            var prefixPos = value.IndexOf(prefixToken);
+            if (prefixPos == -1)
                 return value;
 
             int startIndex = 0;
             var stringBuilder = new StringBuilder();
-            while (dollarPos != -1)
+            while (prefixPos != -1)
             {
-                var openingTokenPos = dollarPos + 1;
+                var openingTokenPos = prefixPos + 1;
                 if (value.Length > openingTokenPos && value[openingTokenPos] == openingToken)
                 {
                     var closingTokenPos = IndexOfClosingToken(value, openingTokenPos, closingToken);
                     Require.That(() => closingTokenPos != -1, $"Could not find matching '{openingToken}' for an '{closingToken}' at position {openingTokenPos}.");
 
-                    var variableName = value.Substring(openingTokenPos + 1, closingTokenPos - openingTokenPos - 1);
+                    var variableNameAndFormat = value.Substring(openingTokenPos + 1, closingTokenPos - openingTokenPos - 1);
                     string resolvedValue;
-                    if (variableName.Length > 1 && variableName[0] == prefixToken && variableName[1] == openingToken)
-                    {
-                        resolvedValue = variableName;
+                    if (variableNameAndFormat.Length > 1 && variableNameAndFormat[0] == prefixToken && variableNameAndFormat[1] == openingToken)
+                    {//handles escape sequence like ${${var}} turning it into ${var}
+                        resolvedValue = variableNameAndFormat;
                     }
                     else
                     {
-                        var variableValue = resolverFunc(variableName);
-                        stringBuilder.Append(value.Substring(startIndex, dollarPos - startIndex));
+                        (var variableNameAndAligment, var format) = variableNameAndFormat.SplitOnFirstOptional(":");
+                        (var variableName, var alignment) = variableNameAndAligment.SplitOnFirstOptional(",");
+
+                        var variableValue = resolverFunc(variableName, variableNameAndFormat.Substring(variableName.Length));
+                        stringBuilder.Append(value.Substring(startIndex, prefixPos - startIndex));
                         resolvedValue = Resolve(variableValue, resolverFunc);
                     }
                     stringBuilder.Append(resolvedValue);
@@ -50,11 +54,11 @@ namespace Qart.Core.Text
                 }
                 else
                 {
-                    stringBuilder.Append(value.Substring(startIndex, dollarPos - startIndex + 1));
-                    startIndex = dollarPos + 1;
+                    stringBuilder.Append(value.Substring(startIndex, prefixPos - startIndex + 1));
+                    startIndex = prefixPos + 1;
                 }
 
-                dollarPos = value.IndexOf(prefixToken, startIndex);
+                prefixPos = value.IndexOf(prefixToken, startIndex);
             }
             stringBuilder.Append(value.Substring(startIndex));
 
--- tests/Qart.Core.Tests/Text/VariableResolverTests.cs
+++ tests/Qart.Core.Tests/Text/VariableResolverTests.cs
@@ -2,12 +2,14 @@
 using Qart.Core.Text;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Qart.Core.Tests.Text
 {
     class VariableResolverTests
     {
         private readonly static IDictionary<string, string> _variables = new Dictionary<string, string> { { "var", "value" } };
+        private readonly static IDictionary<string, object> _variables2 = new Dictionary<string, object> { { "var", -27 }, { "var2", new DateTime(2022, 2, 19, 13, 50, 34) } };
 
         [TestCase("abc", ExpectedResult = "abc")]
         [TestCase("abc$", ExpectedResult = "abc$")]
@@ -19,6 +21,15 @@ namespace Qart.Core.Tests.Text
         public string ResolveSucceeds(string value)
         {
             return VariableResolver.Resolve(value, key => _variables[key]);
+        }
+
+        [TestCase("_${var:X}_", ExpectedResult = "_FFFFFFE5_")]
+        [TestCase("_${var,15}_", ExpectedResult = "_            -27_")]
+        [TestCase("_${var,15:X}_", ExpectedResult = "_       FFFFFFE5_")]
+        [TestCase("${var2:yyyyMMMdd HH:mm:ss}_", ExpectedResult = "2022Feb19 13:50:34_")]
+        public string ResolveFormatSucceeds(string value)
+        {
+            return VariableResolver.Resolve(value, key => _variables2[key]);
         }
 
         [TestCase("abc${")]
